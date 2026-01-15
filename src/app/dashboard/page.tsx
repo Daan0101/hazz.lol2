57704ea1fcfabf150da4c3df90ecfb0558588390
daan0101 <173400@student.talland.nl>
2026-01-15 20:32:39 +0100

    fix: corrected route on isPending on dashboard page

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -10,7 +10,7 @@ export default function DashboardPage() {
 
   useEffect(() => {
     if (!isPending && !session?.user) {
-      router.push("/signin");
+      router.push("/login");
     }
   }, [isPending, session, router]);
 
